56d212ca281e6026a505c41581cd05eea2eb5445
Louis <lightning-faucet@protonmail.com>
2021-10-25 18:09:35 +0000

    Fix auth

---
+++ bin/generateKey.ts
@@ -0,0 +1,12 @@
+// deno run --allow-write generateKey.ts
+
+const key = await crypto.subtle.generateKey(
+  { name: "HMAC", hash: "SHA-512" },
+  true,
+  ["sign", "verify"]
+);
+
+const result = await crypto.subtle.exportKey("jwk", key);
+
+await Deno.writeTextFile("./newKey.json", JSON.stringify(result));
+console.log("New key written to ./newKey.json");
--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -1,15 +1,30 @@
 import type { RouterContext } from "https://deno.land/x/oak/mod.ts";
 import { verify } from "https://deno.land/x/djwt/mod.ts";
 
+const keyData = await Deno.readTextFile("jwk.json");
+
+const key = await crypto.subtle.importKey(
+  "jwk",
+  JSON.parse(keyData),
+  { name: "HMAC", hash: "SHA-512" },
+  true,
+  ["sign", "verify"]
+);
+
+export const JwtConfig = {
+  secretKey: key,
+};
+
 export const validateJWT = async (
-  { request, response }: RouterContext,
-  next: VoidFunction,
+  { request, response, state }: RouterContext,
+  next: VoidFunction
 ) => {
   const auth = request.headers.get("Authorization");
+  if (!auth) return;
   const matches = /Bearer\s*(.*)/.exec(auth);
   let jwt;
 
-  if (matches.length > 1) {
+  if (matches && matches.length > 1) {
     jwt = matches[1];
   }
 
@@ -19,8 +34,9 @@ export const validateJWT = async (
     }
 
     await verify(jwt, key)
-      .then(async () => {
+      .then(async (payload) => {
         console.log("Valid JWT Token! 😎");
+        state.user = payload;
         await next(); // The next() will continue with the excecution
       })
       .catch((e) => {
