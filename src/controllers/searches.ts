33c223bb3b34533c167f6fb4ec72b3a427b92489
Anthony Ettinger <anthony@chovy.com>
2021-11-01 17:06:55 -0700

    add put method for search profile

--- src/controllers/searches.ts
+++ src/controllers/searches.ts
@@ -41,7 +41,7 @@ class Controller {
 
   async delete(context: any) {
     const id = context.params.id;
-    
+
     await db.query("DELETE FROM searches WHERE id = ?", [id]);
     await db.query("DELETE FROM positive WHERE search_id = ?", [id]);
     await db.query("DELETE FROM negative WHERE search_id = ?", [id]);
@@ -62,13 +62,66 @@ class Controller {
   async getOne(context: any) {
     const id = context.state.user.id;
     const search_id = context.params.id;
-    const data = await db.queryEntries(
+    let data = await db.queryEntries(
       "SELECT * FROM searches WHERE user_id = ? AND id = ?",
       [id, search_id],
     );
 
+    data = data.pop();
+
+    const positive = await db.queryEntries('SELECT word FROM positive WHERE search_id = ?', [search_id]);
+
+    const negative = await db.queryEntries('SELECT word FROM negative WHERE search_id = ?', [search_id]);
+
+    data.positive = positive.map(w => w.word);
+    data.negative = negative.map(w => w.word);
     context.response.body = data;
   }
+
+
+  async update(context: any) {
+    const id = context.state.user.id;
+    const body = JSON.parse(await context.request.body().value);
+    const { positive, negative, name } = body;
+    const data: any = {};
+    const search_id = context.params.id;
+
+    // insert name of search
+    await db.query(
+      "UPDATE searches SET name = ?, updated_at = ? WHERE id = ?",
+      [name, new Date().toISOString(), search_id],
+    );
+
+    // add positive keywords
+    await db.query('DELETE from positive WHERE search_id = ?', [search_id]);
+
+    for (let word of positive) {
+      await db.query(
+        "INSERT INTO positive (id, search_id, word) VALUES (?, ?, ?)",
+        [crypto.randomUUID(), search_id, word],
+      );
+    }
+
+    // add negative keywords
+    await db.query('DELETE from negative WHERE search_id = ?', [search_id]);
+
+    for (let word of negative) {
+      await db.query(
+        "INSERT INTO negative (id, search_id, word) VALUES (?, ?, ?)",
+        [crypto.randomUUID(), search_id, word],
+      );
+    }
+
+    data.positive = positive;
+    data.negative = negative;
+    data.name = name;
+
+    context.response.status = 201;
+    context.response.body = data;
+  }
+
+
+
 }
 
 export default new Controller();
--- src/routes/searches.ts
+++ src/routes/searches.ts
@@ -7,7 +7,7 @@ const router = new Router({ prefix: "/api/1" });
 router
   .get("/searches", validateJWT, controller.getAll.bind(controller))
   .get("/searches/:id", validateJWT, controller.getOne.bind(controller))
-  //.put("/searches/:id", validateJWT, controller.update.bind(controller))
+  .put("/searches/:id", validateJWT, controller.update.bind(controller))
   .post("/searches", validateJWT, controller.post.bind(controller))
   .delete("/searches/:id", validateJWT, controller.delete.bind(controller));
 
