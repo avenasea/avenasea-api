92ff51466803003396dfa767b5e7355cea5e1d08
Anthony Ettinger <anthony@chovy.com>
2022-03-06 17:52:18 -0800

    job type

---
+++ src/db/migrations/20220305183519_add_search_type_remove_user_type.ts
@@ -0,0 +1,27 @@
+import {
+  AbstractMigration,
+  Info,
+  ClientSQLite,
+} from "https://deno.land/x/nessie@2.0.5/mod.ts";
+
+export default class extends AbstractMigration<ClientSQLite> {
+  /** Runs on migrate */
+  async up(info: Info): Promise<void> {
+    await this.client.query(`
+				ALTER TABLE users
+					DROP COLUMN type
+			`);
+    await this.client.query(`
+				ALTER TABLE searches
+					ADD COLUMN type TEXT DEFAULT 'job'
+			`);
+  }
+
+  /** Runs on rollback */
+  async down(info: Info): Promise<void> {
+    await this.client.query(`
+				ALTER TABLE searches
+					DROP COLUMN type
+			`);
+  }
+}
