6e6cdf4f57944e75f515d47e4c4adda48d8458ac
Anthony Ettinger <anthony@chovy.com>
2022-03-13 17:19:27 -0700

    remove no-check

--- bin/email.sh
+++ bin/email.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")/.."
 . ~/.zshrc
 pwd
 
-deno run -A --unstable --no-check ./bin/email.ts >> /tmp/grazily.cron.log 2>&1
+deno run -A --unstable ./bin/email.ts >> /tmp/grazily.cron.log 2>&1
