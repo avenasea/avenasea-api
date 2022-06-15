e9d3efbf590cbfab4945e00bce0f726a09581e9c
Louis <60443647+louishuddleston@users.noreply.github.com>
2022-06-15 17:31:40 +0100

    CSV to schema script (#1)
    * create csv to JSONSchema script
    
    * commit csv
    
    * split on modules
    
    * Revert "split on modules"
    
    This reverts commit 72caa93754a8e56f4652ddc478a820796724564b.
    
    * don't allow additional properties

---
+++ bin/csvToSchema.ts
@@ -0,0 +1,38 @@
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { readCSVObjects } from 'https://deno.land/x/csv/mod.ts';
+
+// deno run -A ./bin/csvToSchema.ts --path test.csv
+
+const { path } = parse(Deno.args);
+
+const f = await Deno.open(path || './GBTA.csv');
+
+const schema: any = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  description: '',
+  type: 'object',
+  properties: {},
+  required: [],
+  additionalProperties: false,
+};
+
+const types: any = {
+  Text: 'string',
+  Number: 'number',
+  Date: 'string', // TODO: add format property
+};
+
+for await (const row of readCSVObjects(f)) {
+  const name = row['Field Name'];
+  schema.properties[name] = {
+    field: row['Field'],
+    module: row['Module'],
+    description: row['Description'],
+    type: types[row['Field Type']],
+  };
+  const match = row['Field Size'].match(/(\d+)/g);
+  if (match?.[0]) schema.properties[name].maxLength = parseInt(match[0]);
+  if (match?.[1]) schema.properties[name].minLength = parseInt(match[1]);
+}
+
+Deno.writeTextFileSync('./newSchema.json', JSON.stringify(schema));
