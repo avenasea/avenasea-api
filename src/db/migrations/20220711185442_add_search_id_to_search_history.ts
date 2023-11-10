857eda90b7903e9002701e16297c82547633ad50
Anthony Ettinger <anthony@chovy.com>
2023-11-10 07:14:35 -0800

    update nessie

--- src/db/migrations/20220711185442_add_search_id_to_search_history.ts
+++ src/db/migrations/20220711185442_add_search_id_to_search_history.ts
@@ -2,7 +2,7 @@ import {
   AbstractMigration,
   Info,
   ClientSQLite,
-} from "https://deno.land/x/nessie@2.0.5/mod.ts";
+} from "https://deno.land/x/nessie@2.0.11/mod.ts";
 
 export default class extends AbstractMigration<ClientSQLite> {
   /** Runs on migrate */
